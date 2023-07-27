d42dec979f9573e619b0d7c9df4ab5358df2a30a
Sam <Sambose426@yahoo.com>
2023-07-27 03:06:45 -0600

    finished the algorithm for the quiz

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,38 @@ var startBtn = $(".start-btn");
 var timerSpan = $("#timer-span");
 var introSec = $(".intro");
 var questionSections = $(".question-section");
+var scoreSec = $(".score-screen");
+var scoreSpan = $("#score-span");
 
 // Global variables
-var i = 0;
 var timer = 60;
 var answers = ["b", "b", "a", "c"];
+var i = 0;
+var correct = 0;
 
 function loadNextQuestion(index) {
 	questionSections.eq(index - 1).hide();
 	questionSections.eq(index).show();
 }
 
+function showScore() {
+	var grade = 0;
+	grade = correct * (100 / questionSections.length);
+	scoreSec.show();
+	scoreSpan.text(grade);
+	return grade;
+}
+
 startBtn.on("click", function () {
-	// setInterval(() => {
-	// 	timer--;
-	// 	timerSpan.text(timer);
-	// }, 1000);
+	var timerInterval = setInterval(() => {
+		timer--;
+		timerSpan.text(timer);
+		if (i >= 4) {
+			clearInterval(timerInterval);
+			timerSpan.text(0);
+			setTimeout(showScore, 2700);
+		}
+	}, 1000);
 	introSec.hide();
 	loadNextQuestion(i);
 });
@@ -27,16 +43,19 @@ $(document).on("click", function (event) {
 	if (event.target.dataset.option === "op" && event.target.parentElement.dataset.answered !== "answered" && event.target.id === answers[i]) {
 		console.log("correct");
 		event.target.parentElement.dataset.answered = "answered";
+		event.target.style.backgroundColor = "green";
+		correct++;
 		i++;
-		setInterval(() => {
+		setTimeout(function () {
 			loadNextQuestion(i);
-		}, 5000);
+		}, 3000);
 	} else if (event.target.dataset.option === "op" && event.target.parentElement.dataset.answered !== "answered" && event.target.id !== answers[i]) {
 		console.log("incorrect");
 		event.target.parentElement.dataset.answered = "answered";
+		event.target.style.backgroundColor = "red";
 		i++;
-		setInterval(() => {
+		setTimeout(function () {
 			loadNextQuestion(i);
-		}, 5000);
+		}, 3000);
 	}
 });
